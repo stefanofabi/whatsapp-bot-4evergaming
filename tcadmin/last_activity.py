a3038e5b1ee113e09e5a4ee3d5c2a26d10c77796
Stefano <stefanofabi96@gmail.com>
2023-05-13 16:19:50 -0300

    Corregir la concatenación de la lista de tareas

--- tcadmin/last_activity.py
+++ tcadmin/last_activity.py
@@ -1,19 +1,20 @@
 import config
 import requests
 import template_message
+import json
 
 access = config.db.cursor()
 access.execute("SELECT * FROM tc_users where home_phone <> ''")
 resultUsers = access.fetchall()
 for user in resultUsers:
     # tcadmin guarda los datetime en utc
-    sql = "SELECT task_id, display_name FROM tc_tasks WHERE last_run_time >= date_add(now(), INTERVAL 175 MINUTE) AND user_id = %s"
+    sql = "SELECT * FROM tc_tasks WHERE last_run_time >= date_add(now(), INTERVAL 175 MINUTE) AND user_id = %s"
     access.execute(sql, (user[0],))
     resultTasks = access.fetchall()
 
     firstName = user[10].split(" ")[0]
     lastName = user[11]
-    phone = user[19].replace('.', '9').replace('-', '').replace(' ', '')
+    phone = user[19].replace('-', '').replace(' ', '')
 
     if (user[17] == "AR"): 
         phone = "+549" + phone
@@ -27,10 +28,10 @@ for user in resultUsers:
         phone = "+5939" + phone
     else:
         continue
-    
+
     tasks = ""
     for task in resultTasks:
-        tasks = "[" + str(task[0]) + "] " + task[1] + "\n"
+        tasks = tasks + "[" + str(task[0]) + "] " + task[3] + "\n"
 
     if (tasks == ""):
         continue
@@ -38,6 +39,9 @@ for user in resultUsers:
     messageToSend = template_message.last_activity.format(firstName = firstName,lastName = lastName, phone = phone, tasks = tasks)
     url = config.api_endpoint + '/api/send'
     data = {'phone': phone, 'message': messageToSend, 'token': config.api_token}
-    sendMessage = requests.post(url, json = data)
-
-    print(sendMessage.text.encode("utf-8"))
+    sendMessage = requests.post(url, json = data).json()
+    
+    if (sendMessage["error"]):
+        print("Error al enviar el mensaje al usuario #" + user[0])
+    else:
+        print("Mensaje enviado al usuario #" + user[0]) 
