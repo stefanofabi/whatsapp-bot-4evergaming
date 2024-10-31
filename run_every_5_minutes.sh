ef2d8f80720b0c93f55db1b182082956c191649b
Stefano <stefanofabi96@gmail.com>
2024-10-31 16:07:30 -0300

    Add server started to cron

--- run_every_5_minutes.sh
+++ run_every_5_minutes.sh
@@ -7,3 +7,4 @@ cd /root/whatsapp-bot-4evergaming/whmcs && python3 invoice_paid.py
 cd /root/whatsapp-bot-4evergaming/whmcs && python3 client_area_login.py
 
 cd /root/whatsapp-bot-4evergaming/tcadmin && python3 user_login.py
+cd /root/whatsapp-bot-4evergaming/tcadmin && python3 server_started.py
