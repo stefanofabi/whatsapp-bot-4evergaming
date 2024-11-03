2be99c10529795b2ab7d6505bcc7f98208ce4005
Stefano <stefanofabi96@gmail.com>
2024-11-03 17:14:16 -0300

    Fix commands to pay

--- commands/payment_gateways.js
+++ commands/payment_gateways.js
@@ -26,7 +26,7 @@ async function payWithBankTransfer(invoiceId, userPhone, client) {
     `;
 
     try {
-        const [results] = await db.execute(query, [invoiceId, invoiceId, userPhone]);
+        const [results] = await db.execute(query, [isNaN(invoiceId) ? null : invoiceId, isNaN(invoiceId) ? null : invoiceId, userPhone]);
 
         if (results.length === 0) {
             if (isNaN(invoiceId))
@@ -61,16 +61,16 @@ async function payWithBankTransfer(invoiceId, userPhone, client) {
             }
         } else {
             invoicesMessage = '🤖 Acá están tus facturas pendientes:\n\n';
-            results.forEach(({ id, date, duedate, amount, status }) => {
+            results.forEach(({ id, date, duedate, total, status }) => {
                 if (status !== 'Unpaid') return;
 
-                amount = Math.ceil(amount);
+                total = Math.ceil(total);
 
-                totalSum += amount; 
+                totalSum += total; 
                 const formattedDate = new Date(date).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
                 const formattedDueDate = new Date(duedate).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
 
-                invoicesMessage += `*Factura: #${id}*\nFecha: ${formattedDate}\nVencimiento: ${formattedDueDate}\nMonto: \$${amount}\n\n`;
+                invoicesMessage += `*Factura: #${id}*\nFecha: ${formattedDate}\nVencimiento: ${formattedDueDate}\nMonto: \$${total}\n\n`;
             });     
         }
 
@@ -177,97 +177,6 @@ async function payWithCard(invoiceId, userPhone, client) {
     }
 }
 
-
-async function payWithBankTransfer(invoiceId, userPhone, client) {
-    const db = await connect('whmcs');
-
-    const query = `
-        SELECT 
-            tblinvoices.id, 
-            tblinvoices.date, 
-            tblinvoices.duedate, 
-            tblinvoices.total, 
-            tblinvoices.status, 
-            tblinvoices.paymentmethod 
-        FROM 
-            tblinvoices 
-        INNER JOIN 
-            tblclients 
-        ON 
-            tblinvoices.userid = tblclients.id 
-        WHERE 
-            (tblinvoices.id = ? OR ? IS NULL) AND
-            REPLACE(REPLACE(REPLACE(REPLACE(tblclients.phonenumber, '+', ''), '.', '9'), ' ', ''), '-', '') = ?
-    `;
-
-    try {
-        const [results] = await db.execute(query, [invoiceId, invoiceId, userPhone]);
-
-        if (results.length === 0) {
-            if (isNaN(invoiceId))
-            {
-                await client.sendMessage(userPhone + "@c.us", '🤖 No hay facturas pendientes por pagar');
-            } else {
-                await client.sendMessage(userPhone + "@c.us", '🤖 No existe la factura');
-            }
-
-            await db.end();
-            return;
-        }
-
-        let totalSum = 0;
-        let invoicesMessage = "";
-
-        if (results.length === 1 && !isNaN(invoiceId)) {
-            let { id, date, duedate, total, status } = results[0];
-
-            if (status === 'Unpaid') {
-                total = Math.ceil(total);
-                totalSum += total; 
-                const formattedDate = new Date(date).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
-                const formattedDueDate = new Date(duedate).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
-
-                invoicesMessage += '🤖 Acá está tu factura pendiente:\n\n';
-                invoicesMessage += `*Factura: #${id}*\nFecha: ${formattedDate}\nVencimiento: ${formattedDueDate}\nTotal: \$${total}\n`;
-            } else {
-                await client.sendMessage(userPhone + "@c.us", '🤖 No podés pagar esta factura.\n\n Motivo: Estado de la factura '+status);
-                await db.end();
-                return;
-            }
-        } else {
-            invoicesMessage = '🤖 Acá están tus facturas pendientes:\n\n';
-            results.forEach(({ id, date, duedate, amount, status }) => {
-                if (status !== 'Unpaid') return;
-
-                amount = Math.ceil(amount);
-
-                totalSum += amount; 
-                const formattedDate = new Date(date).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
-                const formattedDueDate = new Date(duedate).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
-
-                invoicesMessage += `*Factura: #${id}*\nFecha: ${formattedDate}\nVencimiento: ${formattedDueDate}\nMonto: \$${amount}\n\n`;
-            });     
-        }
-
-        const totalSumDiscount = Math.ceil(totalSum * 0.90); 
-        invoicesMessage += `\n *Total a pagar (10% de descuento):* ~~\$${totalSum}~~ \$${totalSumDiscount}\n\n`;
-
-        invoicesMessage += `Realiza tu pago con los siguientes datos:\n` +
-                          `*Titular:* Stefano Fabi\n` +
-                          `*CUIT:* 20-39881919-6\n` +
-                          `*CVU:* 0000003100090739102124\n` +
-                          `*Alias:* 4evergaming\n\n` +
-                          `Por favor enviar el comprobante de la transferencia 🙏`;
-
-        await client.sendMessage(userPhone + "@c.us", invoicesMessage);
-        console.log(`[200] Message sent to ${userPhone}`);
-    } catch (err) {
-        console.error('Error fetching invoices:', err);
-    } finally {
-        await db.end();
-    }
-}
-
 async function payWithMercadoPago(invoiceId, userPhone, client) {
     const db = await connect('whmcs');
 
@@ -291,7 +200,7 @@ async function payWithMercadoPago(invoiceId, userPhone, client) {
     `;
 
     try {
-        const [results] = await db.execute(query, [invoiceId, invoiceId, userPhone]);
+        const [results] = await db.execute(query, [isNaN(invoiceId) ? null : invoiceId, isNaN(invoiceId) ? null : invoiceId, userPhone]);
 
         if (results.length === 0) {
             if (isNaN(invoiceId))
@@ -326,16 +235,16 @@ async function payWithMercadoPago(invoiceId, userPhone, client) {
             }
         } else {
             invoicesMessage = '🤖 Acá están tus facturas pendientes:\n\n';
-            results.forEach(({ id, date, duedate, amount, status }) => {
+            results.forEach(({ id, date, duedate, total, status }) => {
                 if (status !== 'Unpaid') return;
 
-                amount = Math.ceil(amount);
+                total = Math.ceil(total);
 
-                totalSum += amount; 
+                totalSum += total; 
                 const formattedDate = new Date(date).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
                 const formattedDueDate = new Date(duedate).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
 
-                invoicesMessage += `*Factura: #${id}*\nFecha: ${formattedDate}\nVencimiento: ${formattedDueDate}\nMonto: \$${amount}\n\n`;
+                invoicesMessage += `*Factura: #${id}*\nFecha: ${formattedDate}\nVencimiento: ${formattedDueDate}\nMonto: \$${total}\n\n`;
             });     
         }
 
@@ -379,7 +288,7 @@ async function payWithUala(invoiceId, userPhone, client) {
     `;
 
     try {
-        const [results] = await db.execute(query, [invoiceId, invoiceId, userPhone]);
+        const [results] = await db.execute(query, [isNaN(invoiceId) ? null : invoiceId, isNaN(invoiceId) ? null : invoiceId, userPhone]);
 
         if (results.length === 0) {
             if (isNaN(invoiceId))
@@ -414,16 +323,16 @@ async function payWithUala(invoiceId, userPhone, client) {
             }
         } else {
             invoicesMessage = '🤖 Acá están tus facturas pendientes:\n\n';
-            results.forEach(({ id, date, duedate, amount, status }) => {
+            results.forEach(({ id, date, duedate, total, status }) => {
                 if (status !== 'Unpaid') return;
 
-                amount = Math.ceil(amount);
+                total = Math.ceil(total);
 
-                totalSum += amount; 
+                totalSum += total; 
                 const formattedDate = new Date(date).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
                 const formattedDueDate = new Date(duedate).toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric' });
 
-                invoicesMessage += `*Factura: #${id}*\nFecha: ${formattedDate}\nVencimiento: ${formattedDueDate}\nMonto: \$${amount}\n\n`;
+                invoicesMessage += `*Factura: #${id}*\nFecha: ${formattedDate}\nVencimiento: ${formattedDueDate}\nMonto: \$${total}\n\n`;
             });     
         }
 
